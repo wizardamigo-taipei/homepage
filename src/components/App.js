73dbdd392104bf5dae75a756211c64fcd978ca26
Yu-Chun Lin <y.lin38@ncl.ac.uk>
2019-09-27 16:23:08 +0800

    menu add semantic ui styling

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,6 @@ const App = () => {
   return (
     <Container>
       <header className="App-header">WizardAmigos Taipei</header>
-<<<<<<< HEAD
         <MenuBar />
         <Router>
           <Route path="/" exact component={Home}/>
@@ -22,15 +21,6 @@ const App = () => {
           <Route path="/news" exact component={News} />
           <Route path="/contact" exact component={Contact} />
         </Router>
-=======
-      <Menu />
-      <Router>
-        <Route path="/" exact component={Home} />
-        <Route path="/events" exact component={Events} />
-        <Route path="/news" exact component={News} />
-        <Route path="/contact" exact component={Contact} />
-      </Router>
->>>>>>> 39a8ebc75ad3bd3aca9e4f6b2d972de9efa15091
     </Container>
   )
 }
