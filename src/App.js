354f3124a59c40c9da52a114dcd4a11d779d6592
ookangzheng <okz5289@gmail.com>
2019-09-27 12:25:01 +0800

    first init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './App.css'
+import { Container } from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  return (
+    <Container>
+      <header className="App-header"></header>
+    </Container>
+  )
+}
+
+export default App
