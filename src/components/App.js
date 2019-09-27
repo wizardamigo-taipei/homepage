a39bb055797a5782b848c84f03542f4a0c082fc5
Yu-Chun Lin <y.lin38@ncl.ac.uk>
2019-09-27 15:49:20 +0800

    fetch menu tap data from API

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import React from 'react'
 import { Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 
+import Menu from './Menu'
+
+
 function App() {
   return (
     <Container>
       <header className="App-header">WizardAmigos Taipei</header>
+      <Menu />
     </Container>
   )
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class Menu extends React.Component {
+    state = { data : [] }
+
+    componentDidMount() {
+        this.renderHome()
+    }
+
+    renderHome = async () => {
+        const response = await fetch('https://okz.moe/wizardamigos/wp-json/menus/v1/menus/2')
+            .then(res => res.json())
+            .then(data => data.items)
+
+        this.setState({ data:response })
+    }
+
+    displayContent = () => {
+        return (
+            <ul>
+                {this.state.data.map((el,index) => (
+                    <li key={index}>{el.title}</li>    
+                ))}
+            </ul>
+        )
+    }
+
+    render() {
+        if (!this.state.data.length === 0) return (<div>Loading...</div>)
+
+        return <div>{this.displayContent()}</div>
+    }
+}
+
+export default Menu
